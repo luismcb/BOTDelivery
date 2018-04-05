20a211c123fba20ef3e98da9e7161b55dec5aa7a
Antonio Zarate <antonio.zarate@cmscloud.pe>
2018-04-05 13:04:33 -0500

    Signed-off-by: Luis Castillo

--- app.js
+++ app.js
@@ -422,6 +422,6 @@ bot.dialog('busqueda',[(session)=>{
 
 
 bot.dialog('consulta',(session)=>{
-   session.send('Aun esta en construccion :(');
+   session.send('Las consultas aun no estan implementadas :(');
    session.endDialog();
 });
